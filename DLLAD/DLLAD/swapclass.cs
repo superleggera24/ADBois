c4c3cb5c2e28b8ed5e3d3df1db1282d436dfbf7d
Frans Orsel <theforsel@live.nl>
2017-02-16 14:12:40 +0100

    Initiation of project

---
+++ DLLAD/DLLAD/swapclass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLAD
+{
+    class swapclass
+    {
+        public static void Swap<T>(ref T val1, ref T val2)
+        {
+            T temp;
+            temp = val1;
+            val1 = val2;
+            val2 = temp;
+        }
+    }
+}
