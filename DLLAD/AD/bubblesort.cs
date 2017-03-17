935f97d53b13d4166bf6bbc524db564f6215b2e4
DennisJoel <dennis.j.dekker@gmail.com>
2017-03-17 11:16:39 +0100

    BubbleSort, swapclass incl. comments geadd in DLL
    BubbleSort.cs
    swapclass.cs
    Added in Project AD

---
+++ DLLAD/AD/bubblesort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AD
+{
+    public static class BubbleSort
+    {
+        public static void BubbleSortArrayList<T>(this T[] arr) where T : IComparable<T>
+        {
+            for (var i = 0; i < arr.Length; i++)
+            {
+                for (var j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j].CompareTo(arr[j + 1]) > 0)
+                    {
+                        swapclass.Swap<T>(ref arr[j], ref arr[j + 1]);
+                    }
+                }
+            }
+        }
+    }
+}
