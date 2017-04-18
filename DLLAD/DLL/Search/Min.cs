c0783b44614731ef88823a94301549909a554626
DennisDekker <dennis.j.dekker@gmail.com>
2017-04-18 13:37:04 +0200

    Comment MinMax

--- DLLAD/DLL/Search/Min.cs
+++ DLLAD/DLL/Search/Min.cs
@@ -8,13 +8,14 @@ namespace AD.Search
 {
     public static class Min<T> where T : IComparable
     {
+        // Deze methode brengt de speler met de laagste Highscore terug.
         public static T Search(T[] array)
         {
             T min = array[0];
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length - 1; i++) //loop door de gehele array
             {
-                if (array[i].CompareTo(min) < 0)
-                min = array[i];
+                if (array[i].CompareTo(min) < 0) // alleen als het huidige punt lager is dan de huidige min, verander de min naar de huidige.
+                    min = array[i];
 
             }
             return min;
